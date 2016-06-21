87e55809c12c525df7de1943c7495d6aff946f38
OneByte <yo_imhere2@163.com>
2016-06-21 23:23:25 +0800

     增加iOS-Core-Animation-Advanced-Techniques学习

---
+++ ZJCoreAnimation/ZJCoreAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZJCoreAnimation
+//
+//  Created by 余志杰 on 16/6/21.
+//  Copyright © 2016年 余志杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
